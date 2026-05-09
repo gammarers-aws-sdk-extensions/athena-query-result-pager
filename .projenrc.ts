522b7315be81924cdf63143b2ef54c394d81827a
yicr <yicr@users.noreply.github.com>
2026-05-09 01:32:48 +0000

    chore(deps): update deps athena-query-result-parser

--- .projenrc.ts
+++ .projenrc.ts
@@ -8,8 +8,8 @@ const project = new typescript.TypeScriptProject({
   typescriptVersion: '5.9.x',
   repository: 'https://github.com/gammarers-aws-sdk-extensions/athena-query-result-pager.git',
   deps: [
-    '@aws-sdk/client-athena@^3',
-    'athena-query-result-parser@^0.2',
+    '@aws-sdk/client-athena@^3.983.0',
+    'athena-query-result-parser@^0.3.1',
   ],
   releaseToNpm: true,
   npmTrustedPublishing: true,
