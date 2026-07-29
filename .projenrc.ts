bc3844c36d7f887d940760a0f205fcde430d804e
yicr <yicr@users.noreply.github.com>
2026-07-29 05:34:45 +0000

    chore: switch package manager from yarn to npm

--- .projenrc.ts
+++ .projenrc.ts
@@ -6,7 +6,7 @@ const project = new typescript.TypeScriptProject({
   name: 'athena-query-result-pager',
   projenrcTs: true,
   typescriptVersion: '6.0.x',
-  packageManager: javascript.NodePackageManager.YARN_CLASSIC,
+  packageManager: javascript.NodePackageManager.NPM,
   repository: 'https://github.com/gammarers-aws-sdk-extensions/athena-query-result-pager.git',
   description: 'Paginate AWS Athena GetQueryResults calls with AWS SDK v3. This library walks NextToken, parses tabular rows with athena-query-result-parser, and forwards MaxResults and QueryResultType from PagerOptions on every GetQueryResults request.',
   keywords: [
